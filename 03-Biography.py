d8d5fe6bf75cf8589d6cf96659ab706d02e3d62b
raqeeb2007 <raqeebisbest@gmail.com>
2025-11-20 17:01:47 +0400

    Update 03-Biography.py
    Updated my code

--- 03-Biography.py
+++ 03-Biography.py
@@ -1,8 +1,18 @@
-Biography = {
-    "Name": "Raqeeb" ,
-    "Hometown" : "Dubai",
-    "Age" : 18
+Name = input("Enter Your Full Name:")
+Hometown = input("Enter Your Hometown:")
 
+while True:
+    Age_input = input("Enter Your Age:")
+    if Age_input.isdigit():
+        Age = int(Age_input)
+        break
+    else:
+        print("Please input a valid number for your Age")
+
+Biography = {
+    "Name" : Name,
+    "Hometown" : Hometown,
+    "Age" : Age
 }
 
 print(Biography["Name"], Biography["Hometown"], Biography["Age"], sep="\n")
